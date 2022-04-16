252c725ac6db2feb0170a25da16ee118d13e57ab
oohyun15 <sakiss4774@gmail.com>
2022-04-16 17:05:59 +0900

    Initialize spring boot

---
+++ src/test/kotlin/com/belveth/bullettime/BulletTimeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.belveth.bullettime
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BulletTimeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
