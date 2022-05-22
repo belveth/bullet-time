18956258d5dc06616bfdff132bb6775a25c72e14
oohyun15 <sakiss4774@gmail.com>
2022-05-22 17:58:15 +0900

    :sparkles: Add user repository

---
+++ src/main/kotlin/com/belveth/bullettime/domain/user/repository/UserRepository.kt
@@ -0,0 +1,9 @@
+package com.belveth.bullettime.domain.user.repository
+
+import com.belveth.bullettime.domain.user.entity.UserEntity
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
+
+interface UserRepository: JpaRepository<UserEntity, Long> {
+  fun getUser(id: Int): Optional<UserEntity>
+}
