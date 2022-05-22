b84f985cf1be00b3a773d7e6315bd8a7d065f5df
oohyun15 <sakiss4774@gmail.com>
2022-05-22 17:58:35 +0900

    :sparkles: Add user controller

---
+++ src/main/kotlin/com/belveth/bullettime/domain/user/UserController.kt
@@ -0,0 +1,17 @@
+package com.belveth.bullettime.domain.user
+
+import com.belveth.bullettime.domain.user.service.UserService
+import com.belveth.bullettime.domain.user.dto.UserDto
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+
+@RestController
+@RequestMapping("/users")
+class UserController(
+    val userService: UserService,
+) {
+  @GetMapping("/{id}")
+  fun getUser(@PathVariable id: Int): UserDto = userService.getUser(id)
+}
