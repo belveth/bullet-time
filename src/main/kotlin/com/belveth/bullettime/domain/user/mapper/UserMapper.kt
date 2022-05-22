da4c61b6147cc3d0690d99352ce112a47468907c
oohyun15 <sakiss4774@gmail.com>
2022-05-22 17:42:47 +0900

    :sparkles: Add user mapper

---
+++ src/main/kotlin/com/belveth/bullettime/domain/user/mapper/UserMapper.kt
@@ -0,0 +1,14 @@
+package com.belveth.bullettime.domain.user.mapper
+
+import com.belveth.bullettime.domain.user.dto.UserDto
+import com.belveth.bullettime.domain.user.entity.UserEntity
+import org.mapstruct.BeanMapping
+import org.mapstruct.Mapper
+import org.mapstruct.NullValuePropertyMappingStrategy
+import org.mapstruct.ReportingPolicy
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+interface UserMapper {
+  @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+  fun dtoFromEntity(userEntity: UserEntity): UserDto
+}
