51107246d63d1a806093e47ec06932c80d1a544b
oohyun15 <sakiss4774@gmail.com>
2022-05-29 15:26:12 +0900

    :sparkles: Add create user dto

---
+++ src/main/kotlin/com/belveth/bullettime/domain/user/dto/CreateUserDto.kt
@@ -0,0 +1,13 @@
+package com.belveth.bullettime.domain.user.dto
+
+import javax.validation.constraints.NotBlank
+
+data class CreateUserDto(
+    @field:NotBlank
+    var name: String?,
+    @field:NotBlank
+    var email: String?,
+    @field:NotBlank
+    var password: String?,
+    var nickname: String?,
+)
