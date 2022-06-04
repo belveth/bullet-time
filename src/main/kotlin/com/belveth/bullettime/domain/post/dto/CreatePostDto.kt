faf8413fc3a5152bda473d183fb31651217d83b2
oohyun15 <sakiss4774@gmail.com>
2022-06-04 17:01:58 +0900

    :sparkles: Add post DTO

--- src/main/kotlin/com/belveth/bullettime/domain/post/dto/CreatePostDto.kt
+++ src/main/kotlin/com/belveth/bullettime/domain/post/dto/CreatePostDto.kt
@@ -3,11 +3,12 @@ package com.belveth.bullettime.domain.post.dto
 import javax.validation.constraints.NotBlank
 
 data class CreatePostDto(
-    @field:NotBlank
-    var name: String?,
-    @field:NotBlank
-    var email: String?,
-    @field:NotBlank
-    var password: String?,
-    var nickname: String?,
+    @field:NotBlank var title: String?,
+    var body: String?,
+    var product: String?,
+    var price: Long?,
+    var category_id: Long?,
+    var image: String?,
+    var post_type: String?,
+    var contract: String?,
 )
--- src/main/kotlin/com/belveth/bullettime/domain/post/dto/PostDto.kt
+++ src/main/kotlin/com/belveth/bullettime/domain/post/dto/PostDto.kt
@@ -1,8 +1,13 @@
 package com.belveth.bullettime.domain.post.dto
 
 data class PostDto(
-  val id: Long?,
-  val name: String?,
-  val email: String?,
-  val nickname: String?,
+    val id: Long?,
+    var body: String?,
+    var product: String?,
+    var price: Long?,
+    var post_type: String?,
+    var category_id: Long?,
+    var image: String?,
+    var contract: String?,
+    var status: Int?,
 )
