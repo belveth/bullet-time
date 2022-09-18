82191a666dc856192c260cf22358b3ab85c34258
oohyun15 <sakiss4774@gmail.com>
2022-09-18 17:36:23 +0900

    :sparkles: Get auth token from request header

--- src/main/kotlin/com/belveth/bullettime/domain/post/PostController.kt
+++ src/main/kotlin/com/belveth/bullettime/domain/post/PostController.kt
@@ -4,6 +4,8 @@ import com.belveth.bullettime.domain.post.dto.CreatePostDto
 import com.belveth.bullettime.domain.post.dto.PostDto
 import com.belveth.bullettime.domain.post.dto.UpdatePostDto
 import com.belveth.bullettime.domain.post.service.PostService
+import com.belveth.bullettime.domain.auth.service.AuthService
+import com.belveth.bullettime.domain.user.service.UserService
 import org.springframework.http.HttpStatus
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
@@ -11,21 +13,34 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("/posts")
 class PostController(
-    val postService: PostService,
+        val postService: PostService,
+        val authService: AuthService,
+        val userService: UserService,
 ) {
-  @GetMapping("/{id}") fun getPost(@PathVariable id: Long): PostDto = postService.getPost(id)
+    @GetMapping("/{id}")
+    fun getPost(
+        @PathVariable id: Long,
+        @RequestHeader("Auth-Token") authToken: String
+    ): PostDto {
+        val userId = authService.deserialize(authToken)
+        val currentUser = userService.getUser(userId)
+        println(currentUser)
+        return postService.getPost(id)
+    }
 
-  @PostMapping()
-  @ResponseStatus(HttpStatus.OK)
-  fun createPost(@Validated @RequestBody createPostDto: CreatePostDto): PostDto =
-      postService.createPost(createPostDto)
+    @PostMapping()
+    @ResponseStatus(HttpStatus.OK)
+    fun createPost(@Validated @RequestBody createPostDto: CreatePostDto): PostDto =
+            postService.createPost(createPostDto)
 
-  @PutMapping("/{id}")
-  @ResponseStatus(HttpStatus.OK)
-  fun updatePoset(@PathVariable id: Long, @Validated @RequestBody updatePostDto: UpdatePostDto): PostDto =
-      postService.updatePost(id, updatePostDto)
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    fun updatePoset(
+            @PathVariable id: Long,
+            @Validated @RequestBody updatePostDto: UpdatePostDto
+    ): PostDto = postService.updatePost(id, updatePostDto)
 
-  @DeleteMapping("/{id}")
-  @ResponseStatus(HttpStatus.OK)
-  fun deletePost(@PathVariable id: Long) = postService.deletePost(id)
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    fun deletePost(@PathVariable id: Long) = postService.deletePost(id)
 }
